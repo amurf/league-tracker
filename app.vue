7546ac36f63e37c764a8ff213c368610f8e6e505
amurf <ash.d.murphy@gmail.com>
2024-12-06 23:29:43 +1100

    feat: add supabase module

--- app.vue
+++ app.vue
@@ -1,6 +1,17 @@
+<script setup>
+const client = useSupabaseClient();
+
+let { data: tournaments } = await useAsyncData("tournaments", async () => {
+    const { data } = await client
+        .from("LeagueTournaments")
+        .select("tournament_name, start_date");
+
+    return data;
+});
+</script>
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <div v-for="tournament in tournaments">
+        {{ tournament.tournament_name }}
+        {{ tournament.start_date }}
+    </div>
 </template>
